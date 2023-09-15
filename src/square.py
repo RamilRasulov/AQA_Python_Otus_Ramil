731508c71623a6e85529040e7934b0cbbde05921
Рамил Расулов <rzrasulov@sbercloud.ru>
2023-09-15 14:08:17 +0300

    HW2 figure, square  + test

---
+++ src/square.py
@@ -0,0 +1,11 @@
+from src.rectangle import Rectangle
+
+
+class Square(Rectangle):
+        def __init__(self, side_a):
+            if side_a <= 0:
+                raise ValueError("Can not create Square")
+            super().__init__(side_a, side_a)
+            self.side_a = side_a
+            self.name = f"Square {side_a}"
+
--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,9 +1,11 @@
-from src.Rectangle import Rectangle
+from src.rectangle import Rectangle
 import pytest
 
-@pytest.mark.parametrize(("side_a", "side_b", "area", "perimeter"),
-                         [(8,10,80,36),
-                          (2,4,8,12)])
+
+@pytest.mark.parametrize(
+    ("side_a", "side_b", "area", "perimeter"),
+    [(8, 10, 80, 36), (2, 4, 8, 12)],
+)
 def test_rectangle(side_a, side_b, area, perimeter):
     r = Rectangle(side_a, side_b)
     assert r.name == f"Rectangle {side_a} and {side_b}"
@@ -11,10 +13,10 @@ def test_rectangle(side_a, side_b, area, perimeter):
     assert r.get_perimeter() == perimeter
 
 
-@pytest.mark.parametrize(("side_a", "side_b", "area", "perimeter"),
-                         [(-8,-10,80,-36),
-                          (0,0,0,0),
-                          (2,4,8,12)])
+@pytest.mark.parametrize(
+    ("side_a", "side_b", "area", "perimeter"),
+    [(-8, -10, 80, -36), (0, 0, 0, 0), (2, 4, 8, 12)],
+)
 def test_rectangle_negative(side_a, side_b, area, perimeter):
     with pytest.raises(ValueError):
         r = Rectangle(side_a, side_b)
